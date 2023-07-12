dc019e97206af4f4954579725bfe9b81a1be83dc
kriskumar2020 <129244649+kriskumar2020@users.noreply.github.com>
2023-07-12 21:36:21 +0530

    Create First Commit Program 2

---
+++ Chapter01/Program002.c
@@ -0,0 +1,18 @@
+//Program 2 WAP to Divide and Get Quotient and Remainder
+#include <stdio.h>
+int main(){
+    int divisor,dividend,quotient,remainder;
+    printf("Enter Dividend :");
+    scanf("%d",&dividend);
+    printf("Enter Divisor :");
+    scanf("%d",&divisor);
+    
+    //dividend = divisor*quotient+remainder;
+    quotient = dividend/divisor;
+    remainder = dividend-(divisor*quotient);
+    printf("Dividend is : %d",dividend);
+    printf("\nDivisor is  : %d",divisor);
+    printf("\nQuotient is : %d",quotient);
+    printf("\nReminder is : %d",remainder);
+    return 0;
+}
