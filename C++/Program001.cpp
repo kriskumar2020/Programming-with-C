393f2acd6407eb7ef808d52316af50677cf0bbd1
kriskumar2020 <129244649+kriskumar2020@users.noreply.github.com>
2023-07-14 07:47:01 +0530

    Create First Commit Program 1

---
+++ C++/Program001.cpp
@@ -0,0 +1,12 @@
+//Program 1 WAP to Print Hello World
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Hello World ,Learn C++ ";
+    cout<<"\nSare Jahan se Achacha Hindustan Hamara";
+    cout<<"\n Hindustan Jindabaad";
+
+    return 0;
+}
