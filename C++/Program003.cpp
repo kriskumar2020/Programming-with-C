de0479e12dabb3648dc71161273ee6e43b1663a3
kriskumar2020 <129244649+kriskumar2020@users.noreply.github.com>
2023-07-15 16:45:13 +0530

    Create First Commit Program 3

---
+++ C++/Program003.cpp
@@ -0,0 +1,12 @@
+//Lecture 3 WAP to Print ASCII value of the Alphabet
+#include<iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    cout<< " Write an alphabet:";
+    cin>> ch ;
+    cout<<"The ASCII value of the Alphabet"<<ch<<"is:"<<int(ch);
+
+    return 0;
+}
